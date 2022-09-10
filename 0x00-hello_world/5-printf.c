2087885d8fe49cdb67ec9ea277c145f56b674dac
Tellajamiu <Tellajamiu.tj@gmail.com>
2022-09-10 07:33:21 -0700

    my first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,7 @@
  * Return: Always 0 (succes)
  */
 int main(void)
-{	printf("with proper grammer, but the outcome is a piece of art,\n");
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
