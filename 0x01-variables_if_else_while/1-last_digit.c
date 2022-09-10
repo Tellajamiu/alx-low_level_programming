f6e5847a6b64e11b5e2f62f91748c2e2e795533a
Tellajamiu <Tellajamiu.tj@gmail.com>
2022-09-10 16:35:07 -0700

    my first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,12 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 
+/* betty style doc for function main goes there */
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
