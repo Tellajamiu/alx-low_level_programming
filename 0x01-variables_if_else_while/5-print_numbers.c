04b453990c7148660f5abe2767892bac7a365d6b
Tellajamiu <Tellajamiu.tj@gmail.com>
2022-09-11 13:48:33 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	char l;
+
+	l = '0';
+	while (l <= '9')
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
