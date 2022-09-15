0229e267aac156845939c13a91045d94f8939a23
Tellajamiu <Tellajamiu.tj@gmail.com>
2022-09-14 22:25:17 -0700

    my first commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,19 +4,19 @@
  * print_sign - check for integer sign
  * @n: integer to check
  *
- * Return: 0 or 1
+ * Return: 0 or 1 or -1
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(44 + 0);
+		_putchar(43 + 0);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar(0 + 46);
+		_putchar(0 + 48);
 		return (0);
 	}
 	else
