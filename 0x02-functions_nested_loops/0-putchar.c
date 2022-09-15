497d629efafa5512f5fc72c4865bf5b18222b971
Tellajamiu <Tellajamiu.tj@gmail.com>
2022-09-14 19:20:18 -0700

    my first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - print putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchsr('t');
+	_putchar('c');
+	_putchar('h');
+	_Putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+
+	return (0);
+]
