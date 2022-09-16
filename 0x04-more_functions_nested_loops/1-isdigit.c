fb8f8b017719c652dd77d6403daf639b1d1d83e5
Tellajamiu <Tellajamiu.tj@gmail.com>
2022-09-16 05:47:26 -0700

    my first commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _isdigit - check for characters that are digit
+ * @c: character to check
+ *
+ * Return: 1 if c is a digit else 0
+ */
+
+int  _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
