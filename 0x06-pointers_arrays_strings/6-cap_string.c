b64cb594a555f123609533ce697bc43d876154d7
Tellajamiu <Tellajamiu.tj@gmail.com>
2022-09-23 05:17:11 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,28 +7,100 @@
  *
  * Return: adress of string
  */
+
 char *cap_string(char *s)
 {
-	int i = 0, j;
-	char a[] = " \t\n,;.!?\"(){}";
+	int i = 0;
 
-	while (*(s + i))
+	while (s[i] != '\0')
 	{
-		if (*(s + i) >= 'a' && *(s + i) >= 'z')
-		{
-			if (i == 0)
-				*(s + i) -= 'a' - 'A';
-			else
-			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a[j] == *(s + i - 1))
-						*(s + i) -= 'a' - 'A';
-				}
-			}
-		}
-		i++;
+		look_no_sep(s, &i);
+		if (is_letter(s[i]))
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] -= 32;
+		look_sep(s, &i);
 	}
 	return (s);
 }
 
+
+
+/**
+ * look_no_sep - compares if input is not a separator listed in sep
+ * @s: pointer to string
+ *  @i: pointer to increment
+ *
+ *   Return: void1 if foun a no-sep, 0 if reached the string limit
+ */
+
+int look_no_sep(char *s, int *i)
+{
+	int j = 0;
+	int k = *i;
+	char sep[14] = " \t\n,;.!?\"(){}";
+	char no_sep = 0;
+
+	while (s[k] != '\0' && !no_sep)
+	{
+		j = 0;
+		while (j < 13 && !no_sep)
+		{
+			if (s[k] != sep[j])
+			{
+				no_sep = 1;
+				return (1);
+			}
+			j++;
+		}
+		k++;
+	}
+	*i = k;
+	return (0);
+}
+
+/**
+ * look_sep - compares if input is a separator listed in sep
+ * @s: pointer to string
+ * @i: pointer to increment
+ *
+ *  Return: void
+ */
+
+void look_sep(char *s, int *i)
+{
+	int j = 0;
+	int = *i;
+	char sep[14] = " \t\n,;.!?\"(){}";
+	char is_sep = 0;
+
+	while (s[k] != '\0' && !is_sep)
+	{
+		j = 0;
+		while (j < 13)
+		{
+			if (s[k] == sep[j])
+			{
+				*i = k;
+				is_sep = 1;
+			}
+			j++;
+		}
+		k++;
+	}
+	*i = k;
+}
+
+
+/**
+ * is_letter - compares if input is in the range of ascii letters
+ * @c: character to be compared
+ *
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+
+int is_letter(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
