8fa61b86d0d979abd0016c5c47626306ba7a5404
Tellajamiu <Tellajamiu.tj@gmail.com>
2022-09-29 13:51:10 -0700

    0-puts-recursion.c

---
+++ 0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * _puts_recursion - a recursive function that print a string
+ * @s: input string to be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
